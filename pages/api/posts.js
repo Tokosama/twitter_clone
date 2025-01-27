f1e73473652e05abf93f4965aca62567d0789b1e
Tokosama <anouartoko@gmail.com>
2025-01-28 00:27:48 +0100

    Gestion du bug au api/post gestion du cas ou posts est null avec ?

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -20,11 +20,11 @@ export default async function handler(req, res) {
       let searchFilter;
       if (!author && !parent) {
         const myFollows = await Follower.find({
-          source: session.user.id,
+          source: session?.user.id,
         }).exec();
         const idsOfPeopleIFollow = myFollows.map((f) => f.destination);
         console.log({ idsOfPeopleIFollow });
-        searchFilter = { author: [...idsOfPeopleIFollow, session.user.id] };
+        searchFilter = { author: [...idsOfPeopleIFollow, session?.user.id] };
       }
       if (author) {
         searchFilter = { author };
