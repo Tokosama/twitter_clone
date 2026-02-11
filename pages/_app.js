2e4f522340756f18446cbf62ce6dce03245076fc
Tokosama <anouartoko@gmail.com>
2026-02-11 18:56:55 +0100

    Ping setup to keep Monngo alive after long time.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-import TimeAgo from 'javascript-time-ago';
-
-import en from 'javascript-time-ago/locale/en';
+import TimeAgo from "javascript-time-ago";
+import "@/lib/init";
+import en from "javascript-time-ago/locale/en";
 
 TimeAgo.addDefaultLocale(en);
-
 
 export default function App({
   Component,
